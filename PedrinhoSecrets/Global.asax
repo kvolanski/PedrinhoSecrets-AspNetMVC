202b28594be04a2c42a9aa6e5ef5b6a3c07a8bc0
Kevin <kvolanski@gmail.com>
2018-11-17 17:38:48 -0200

    Adicionando arquivos

---
+++ PedrinhoSecrets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PedrinhoSecrets.MvcApplication" Language="C#" %>
---
+++ PedrinhoSecrets/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PedrinhoSecrets.Models
+{
+
+    [Table("USUARIO")]
+    public class Usuario
+    {
+
+        [Key]
+        public int usuarioId { get; set; }
+        public string nome { get; set; }
+        public string login { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+
+    }
+}
